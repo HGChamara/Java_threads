967c9e504087645bf280a33b020a2c48d74a798d
HGChamara <hgchamara1991@gmail.com>
2018-02-20 08:45:32 +0530

    Thread Runnable edit

--- src/main/java/thread_runnable/ThreadTest.java
+++ src/main/java/thread_runnable/ThreadTest.java
@@ -7,8 +7,11 @@ public class ThreadTest
 		ThreadRunnable thread1 = new ThreadRunnable("Thread 1");
 		thread1.start();
 		
-		ThreadRunnable thread2 = new ThreadRunnable("Thread 1");
+		ThreadRunnable thread2 = new ThreadRunnable("Thread 2");
 		thread2.start();
+		
+		ThreadRunnable thread3 = new ThreadRunnable("Thread 3");
+		thread3.start();
 	}
 	
 }
